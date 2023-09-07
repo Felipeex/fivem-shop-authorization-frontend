e2484af2617480a6f75d4a3b13b90a13967b5a73
Felipe Lima <70120728+Felipeex@users.noreply.github.com>
2023-09-07 17:51:18 -0300

    Add link on product

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,6 +4,7 @@ import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import { cookies } from "next/headers";
 import { verifyPlan } from "../utils/verifyPlan";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Dashboard - Autenticação",
@@ -21,15 +22,19 @@ export default async function Page() {
   return (
     <section className="flex">
       {plan.data && (
-        <div className="rounded-[10px] border border-[#5F71CB] p-5 flex flex-col bg-[#2E3035]">
-          <h2 className="font-normal text-[16px] leading-none">Meu produto</h2>
-          <span className="text-[#75808A] font-normal text-[16px] leading-none">
-            Clique ao botão abaixo
-          </span>
-          <button className="mt-[15px] bg-[#5F71CB] px-[30px] py-3 rounded-md transition-colors hover:bg-[#485598]">
-            Criar meu primeiro produto
-          </button>
-        </div>
+        <Link href="dashboard/product/create">
+          <div className="rounded-[10px] border border-[#5F71CB] p-5 flex flex-col bg-[#2E3035]">
+            <h2 className="font-normal text-[16px] leading-none">
+              Meu primeiro produto
+            </h2>
+            <span className="text-[#75808A] font-normal text-[16px] leading-none">
+              Clique ao botão abaixo
+            </span>
+            <button className="mt-[15px] bg-[#5F71CB] px-[30px] py-3 rounded-md transition-colors hover:bg-[#485598]">
+              Criar meu primeiro produto
+            </button>
+          </div>
+        </Link>
       )}
     </section>
   );
