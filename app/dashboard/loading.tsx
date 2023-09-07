d82563395360d31382b1d96d2bd477b4db3ed2a1
Felipe Lima <70120728+Felipeex@users.noreply.github.com>
2023-09-07 17:41:59 -0300

    feat: remove loading's comment

--- app/dashboard/loading.tsx
+++ app/dashboard/loading.tsx
@@ -1,5 +1,4 @@
 export default function Loading() {
-  // You can add any UI inside Loading, including a Skeleton.
   return (
     <span className="w-full icon-[eos-icons--loading] text-3xl mx-auto mt-[20vh]" />
   );
