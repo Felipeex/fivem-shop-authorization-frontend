53f482557fc6b6168e774eb47949e90f800b0d61
Felipe Lima <70120728+Felipeex@users.noreply.github.com>
2023-09-07 01:49:09 -0300

    chore: icon new lib

--- components/next-auth/discord-button.tsx
+++ components/next-auth/discord-button.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { signIn } from "next-auth/react";
+
 export function DiscordButton() {
   function handleDiscord(e: React.MouseEvent<HTMLButtonElement>) {
     e.preventDefault();
@@ -9,9 +10,9 @@ export function DiscordButton() {
   return (
     <button
       onClick={(e) => handleDiscord(e)}
-      className="py-2 px-12 bg-[#516BF5] rounded-lg flex items-center justify-center cursor-pointer"
+      className="py-2 px-12 bg-[#516BF5] rounded-lg flex items-center justify-center cursor-pointer transition-colors hover:bg-[#485598] disabled:opacity-50"
     >
-      <span className="icon-[ic--baseline-discord] text-[50px]"></span>
+      <span className="icon-[ic--baseline-discord] text-[50px]" />
     </button>
   );
 }
