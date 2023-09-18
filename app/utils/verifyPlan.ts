b4804b7f008086a09035721e756a574f21f21449
Felipe Lima <70120728+Felipeex@users.noreply.github.com>
2023-09-18 10:17:54 -0300

    refactor: cookie only function.

--- app/utils/verifyPlan.ts
+++ app/utils/verifyPlan.ts
@@ -4,9 +4,5 @@ import { AuthApi } from "../services/auth-api";
 export async function verifyPlan(
   cookie: string
 ): Promise<AxiosResponse<any, any>> {
-  return await AuthApi("/me/plan", {
-    headers: {
-      Authorization: `Bearer ${cookie}`,
-    },
-  });
+  return await AuthApi(cookie)("/me/plan");
 }
